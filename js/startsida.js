5e306e67e1cda4fbf73695e35a0c61afc5e81b2b
jaxhusen <jaxhusen@hotmail.com>
2021-12-09 15:13:21 +0100

    johanna kraschar

--- js/startsida.js
+++ js/startsida.js
@@ -86,7 +86,7 @@ function createSnowFlake() {
  */
 
 
-Width = 1400; // bredd som den snöar på
+/* Width = 1400; // bredd som den snöar på
 Height = 2000; // höjden som den snöar på
 Count = 100; // antal snöflingor 
 MaxStep = 2; // max steg i rörelsen
@@ -103,8 +103,8 @@ snowFlake = new Array();
 for (i = 0; i < Count; i++) {
 left[i] = Math.random() * Width;
 up[i] = Math.random() * Height;
-right[i] = MinStep + Math.random() *-MaxStep;
-down[i] = MinStep + Math.random() *MaxStep;
+right[i] = MinStep + Math.random()* -MaxStep;
+down[i] = MinStep + Math.random()* MaxStep;
 snowFlake[i] = MinFlake + Math.random() *MaxFlake;
  
 document.write("<div id='Obj" + i + "' style='position:absolute; left:0px; top:-20px; z-index:10000; visibility:hidden; color:#fff; font-weight:normal; font-family:Verdana; font-size:"+ snowFlake[i]+"pt'>•</div>");
@@ -132,5 +132,51 @@ setTimeout("animate()", 50);// hastighet
 setTimeout("animate()", 1000);
 
 
+ */
 
 
+
+Width =1500; // ange bredden på sidan som ska snöas in...
+Height =2000; // ange höjden på sidan som ska snöas in...
+Count =25; // antal snöflingor OBS! ange inte för stort antal!
+MaxStep=3; // max steg i rörelsen
+MinStep=1; // min steg i rörelsen
+MaxFlake=10; // max storlek på snöflinga
+MinFlake=4; // min storlek på snöflinga
+ 
+PosX = new Array();
+PosY = new Array();
+StepX = new Array();
+StepY = new Array();
+StarSize=new Array();
+ 
+for (i = 0; i < Count; i++) {
+PosX[i] = Math.random()*Width;
+PosY[i] = Math.random()*Height;
+StepX[i] =MinStep+Math.random()* -MaxStep;
+StepY[i] =MinStep+Math.random()* MaxStep;
+StarSize[i]=MinFlake+Math.random() * MaxFlake;
+ 
+document.write("<div id='Obj" + i + "' style='position:absolute; left:0px; top:-20px; z-index:10000; visibility:hidden; color:#fff; font-weight:normal; font-family:Verdana; font-size:"+StarSize[i]+"pt'>•</div>");
+}
+ 
+function animate() {
+for (i = 0; i < Count; i++) {
+PosY[i] += StepY[i];
+PosX[i] += StepX[i];
+ 
+document.getElementById("Obj"+i).style.visibility = "visible";
+if (PosY[i] > Width || PosX[i] > Height || PosX[i]<0) { // starta om från toppen av sidan när mitten nås
+PosX[i] = Math.random()*Width; ///2
+PosY[i] = -20;
+StepX[i] = MinStep+Math.random()* -MaxStep;
+StepY[i] = MinStep+Math.random()* MaxStep;
+}
+ 
+document.getElementById("Obj"+i).style.top = PosY[i] + "px";
+document.getElementById("Obj"+i).style.left = PosX[i] + "px";
+}
+setTimeout("animate()", 50);// hastighet
+}
+ 
+setTimeout("animate()", 1000);
